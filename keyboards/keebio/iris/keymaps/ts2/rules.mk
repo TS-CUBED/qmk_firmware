a269c32956d59aaabf0fa502466a8ca231071b26
Torsten Schenkel <t.schenkel@shu.ac.uk>
2024-01-20 19:02:05 +0000

    add own keymap

---
+++ keyboards/keebio/iris/keymaps/ts2/rules.mk
@@ -0,0 +1,23 @@
+SRC += features/achordion.c
+
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes      # Audio control and System control(+450)
+CONSOLE_ENABLE = no        # Console for debug(+400)
+COMMAND_ENABLE = no        # Commands for debug and configuration
+NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+MIDI_ENABLE = no           # MIDI controls
+AUDIO_ENABLE = no          # Audio output on port C6
+BLUETOOTH_ENABLE = no      # Enable Bluetooth with the Adafruit EZ-Key HID
+GRAVE_ESC_ENABLE = no
+
+CAPS_WORD_ENABLE = yes
+
+MOUSE_ENABLE = yes
+COMBO_ENABLE = yes
+TAP_DANCE_ENABLE = no
+LTO_ENABLE = yes
+
+DYNAMIC_TAPPING_TERM_ENABLE = yes
