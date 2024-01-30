8900144da1c9aa95eec4aab63a67fd6be4302eb1
Torsten Schenkel <t.schenkel@shu.ac.uk>
2024-01-30 23:57:40 +0000

    add CTL_G key

--- keyboards/keebio/iris/keymaps/ts/keymap.c
+++ keyboards/keebio/iris/keymaps/ts/keymap.c
@@ -32,7 +32,7 @@
 #define CUT      LCTL(KC_X)
 #define COPY     LCTL(KC_C)
 #define PASTE    LCTL(KC_V)
-
+#define CTL_G    LCTL(KC_G)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
                                                                       KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_DEL,
                                                                   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-                                                                     OSL(MCR), HOME_A,  HOME_S,  HOME_D,  HOME_F,   KC_G,                               KC_H,   HOME_J,  HOME_K,  HOME_L,  HOME_SC, KC_QUOT,
+                                                                      CTL_G,   HOME_A,  HOME_S,  HOME_D,  HOME_F,   KC_G,                               KC_H,   HOME_J,  HOME_K,  HOME_L,  HOME_SC, KC_QUOT,
                                                                   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
                                                                      CW_TOGG,  HOME_Z,   KC_X,    KC_C,    KC_V,    KC_B,   KC_BSPC,          KC_ESC,   KC_N,    KC_M,   KC_COMM, KC_DOT,  HOME_SL, CW_TOGG,
                                                                   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
